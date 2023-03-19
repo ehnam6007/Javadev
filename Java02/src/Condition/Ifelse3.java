2e3fe37e867ab5f40a42ef5014fcb5d0cac0792f
ehnam6007 <eunhye911031@naver.com>
2023-03-19 13:08:05 +0900

    2023.03.19 Java day02

--- Java02/src/Condition/Ifelse3.java
+++ Java02/src/Condition/Ifelse3.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class Ifelse3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int year=0;
+		int age=0;
 		int score=0;
 		int passwd=0;
 		int yyear=0;
 		
+		//나이
 		System.out.println("나이를 입력해 주세요 ↓↓↓");
-		year = sc.nextInt();
-		if(year>=20) {System.out.println("성인");}
-		else if(year>=14){System.out.println("청소년");}
-		else if(year<14) {System.out.println("어린이");}
+		age = sc.nextInt();
+		if(age>=20) {System.out.println("성인");}
+		else if(age>=14){System.out.println("청소년");}
+		else if(age<14) {System.out.println("어린이");}
 		
+		
+		//점수
 		System.out.println("점수를 입력해 주세요 ↓↓↓");
 		score=sc.nextInt();
 		if(score>=90) {System.out.println("학점 A");}
@@ -23,14 +26,19 @@ public class Ifelse3 {
 		else if(score>=70){System.out.println("학점 C");}
 		else if(score<70){System.out.println("학점 F");}
 		
+		
+		//패스워드
 		System.out.println("패스워드를 입력해 주세요 ↓↓↓");
 		passwd=sc.nextInt();
 		if(passwd==12345) {System.out.println("비밀번호가 맞습니다.");}
 		else{System.out.println("Error");}
 		
+		
+		
+		//윤년
 		System.out.println("년도를 입력하세요 ↓↓↓");
 		yyear=sc.nextInt();
-		if((yyear%4)==0) {System.out.println("윤년");}
+		if((yyear%4)==0 && (yyear%100)!=0) {System.out.println("윤년");}
 		else {System.out.println("윤년아님");}
 		
 	}
